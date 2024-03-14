45cd1ee5727f54f64bd6eafdff05e329a37461f2
Victor Perez <vparmenta@uma.es>
2024-03-14 12:30:52 +0100

    Test matriculados negativos en Grupo

--- src/test/java/clubdeportivo/GrupoTest.java
+++ src/test/java/clubdeportivo/GrupoTest.java
@@ -9,11 +9,24 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class GrupoTest {
 
     @Test
-    public void GrupoConstructor_nPlazasNegative_ThrowsClubException(){
+    public void GrupoConstructor_nPlazasNegativaOrCero_LanzaClubException(){
         assertThrows(ClubException.class,
                 () -> new Grupo("test", "test", -1, 0, 1),
                 "Con plazas negativas, se deberia lanzar una ClubException.");
+        assertThrows(ClubException.class,
+                () -> new Grupo("test", "test", 0, 0, 1),
+                "Con cero plazas, se deberia lanzar una ClubException.");
     }
+
+    @Test
+    public void GrupoConstructor_matriculadosNegativos_LanzaClubException(){
+        assertThrows(ClubException.class,
+                () -> new Grupo("test", "test", 1, -1, 1),
+                "Con matriculados negativos, se deberia lanzar una ClubException");
+    }
+
+    
+
 
 
 
