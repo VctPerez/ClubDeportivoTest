610f24b544e36f5d4ab6f3afc9f22e2abba4c5f9
DavidBuenoC <davidbuenocarmona@gmail.com>
2024-04-16 12:04:54 +0200

    Create TournamentSelectionTest.java

---
+++ EvolutionaryAlgorithm_P4/src/test/java/org/mps/TournamentSelectionTest.java
@@ -0,0 +1,99 @@
+package org.mps;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mps.selection.TournamentSelection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TournamentSelectionTest {
+
+    TournamentSelection tournamentSelection;
+
+    @BeforeEach
+    public void setUp() throws EvolutionaryAlgorithmException {
+        tournamentSelection = new TournamentSelection(5);
+    }
+
+    @Test
+    @DisplayName("Constructor positive size")
+    public void Constructor_PositiveSize_DoesNotThrowException() {
+        int validSize = 5;
+
+        assertDoesNotThrow(() -> new TournamentSelection(validSize));
+    }
+
+    @Test
+    @DisplayName("Constructor 0 size")
+    public void Constructor_Size0_ThrowsException() {
+        int invalidSize = 0;
+
+        assertThrows(EvolutionaryAlgorithmException.class, () -> new TournamentSelection(invalidSize));
+    }
+
+    @Test
+    @DisplayName("Constructor negative size")
+    public void Constructor_NegativeSize_ThrowsException() {
+        int invalidSize = -1;
+
+        assertThrows(EvolutionaryAlgorithmException.class, () -> new TournamentSelection(invalidSize));
+    }
+
+    @Test
+    @DisplayName("Select array null")
+    public void Select_ArrayNull_ThrowsException() {
+        int[] invalidArray = null;
+
+        assertThrows(EvolutionaryAlgorithmException.class, () -> tournamentSelection.select(invalidArray));
+    }
+
+    @Test
+    @DisplayName("Select empty array")
+    public void Select_EmptyArray_ThrowsException() {
+        int[] invalidArray = new int[0];
+
+        assertThrows(EvolutionaryAlgorithmException.class, () -> tournamentSelection.select(invalidArray));
+    }
+
+    @Test
+    @DisplayName("Select empty array")
+    public void Select_ValidArray_ReturnsArray() throws EvolutionaryAlgorithmException {
+        int[] validArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        int[] selected = tournamentSelection.select(validArray);
+
+        assertArrayEquals(selected, );
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
