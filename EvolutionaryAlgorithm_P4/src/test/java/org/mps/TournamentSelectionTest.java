e767738c2a79a87b10fecd69ccc65b5997267966
DavidBuenoC <davidbuenocarmona@gmail.com>
2024-04-19 10:56:28 +0200

    Fixed display names

--- EvolutionaryAlgorithm_P4/src/test/java/org/mps/TournamentSelectionTest.java
+++ EvolutionaryAlgorithm_P4/src/test/java/org/mps/TournamentSelectionTest.java
@@ -2,6 +2,7 @@ package org.mps;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mps.selection.TournamentSelection;
 
@@ -16,54 +17,62 @@ public class TournamentSelectionTest {
         tournamentSelection = new TournamentSelection(5);
     }
 
-    @Test
-    @DisplayName("Constructor positive size")
-    public void Constructor_PositiveSize_DoesNotThrowException() {
-        int validSize = 5;
+    @Nested
+    class Contructor{
+        @Test
+        @DisplayName("Doesn't throws exception")
+        public void Constructor_PositiveSize_DoesNotThrowException() {
+            int validSize = 5;
 
-        assertDoesNotThrow(() -> new TournamentSelection(validSize));
+            assertDoesNotThrow(() -> new TournamentSelection(validSize));
+        }
+
+        @Test
+        @DisplayName("Throws exception if size is 0")
+        public void Constructor_Size0_ThrowsException() {
+            int invalidSize = 0;
+
+            assertThrows(EvolutionaryAlgorithmException.class, () -> new TournamentSelection(invalidSize));
+        }
+
+        @Test
+        @DisplayName("Throws exception if negative size")
+        public void Constructor_NegativeSize_ThrowsException() {
+            int invalidSize = -1;
+
+            assertThrows(EvolutionaryAlgorithmException.class, () -> new TournamentSelection(invalidSize));
+        }
     }
 
-    @Test
-    @DisplayName("Constructor 0 size")
-    public void Constructor_Size0_ThrowsException() {
-        int invalidSize = 0;
 
-        assertThrows(EvolutionaryAlgorithmException.class, () -> new TournamentSelection(invalidSize));
-    }
+    @Nested
+    class Select {
 
-    @Test
-    @DisplayName("Constructor negative size")
-    public void Constructor_NegativeSize_ThrowsException() {
-        int invalidSize = -1;
+        @Test
+        @DisplayName("Throws Exception if array is null")
+        public void Select_ArrayNull_ThrowsException() {
+            int[] invalidArray = null;
 
-        assertThrows(EvolutionaryAlgorithmException.class, () -> new TournamentSelection(invalidSize));
-    }
+            assertThrows(EvolutionaryAlgorithmException.class, () -> tournamentSelection.select(invalidArray));
+        }
 
-    @Test
-    @DisplayName("Select array null")
-    public void Select_ArrayNull_ThrowsException() {
-        int[] invalidArray = null;
+        @Test
+        @DisplayName("Throws Exception if array is empty")
+        public void Select_EmptyArray_ThrowsException() {
+            int[] invalidArray = new int[0];
 
-        assertThrows(EvolutionaryAlgorithmException.class, () -> tournamentSelection.select(invalidArray));
-    }
+            assertThrows(EvolutionaryAlgorithmException.class, () -> tournamentSelection.select(invalidArray));
+        }
 
-    @Test
-    @DisplayName("Select empty array")
-    public void Select_EmptyArray_ThrowsException() {
-        int[] invalidArray = new int[0];
+        @Test
+        @DisplayName("Returns array if input is valid")
+        public void Select_ValidArray_ReturnsArray() throws EvolutionaryAlgorithmException {
+            int[] validArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        assertThrows(EvolutionaryAlgorithmException.class, () -> tournamentSelection.select(invalidArray));
-    }
+            int[] selected = tournamentSelection.select(validArray);
 
-    @Test
-    @DisplayName("Select empty array")
-    public void Select_ValidArray_ReturnsArray() throws EvolutionaryAlgorithmException {
-        int[] validArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        int[] selected = tournamentSelection.select(validArray);
-
-        assertEquals(selected.length,validArray.length);
+            assertEquals(selected.length,validArray.length);
+        }
     }
 
 
