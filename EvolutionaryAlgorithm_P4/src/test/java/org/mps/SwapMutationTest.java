a9b7d18bb4d91efdde347b6b9f720bdf74abfb7b
DavidBuenoC <davidbuenocarmona@gmail.com>
2024-04-19 10:59:30 +0200

    Update SwapMutationTest.java

--- EvolutionaryAlgorithm_P4/src/test/java/org/mps/SwapMutationTest.java
+++ EvolutionaryAlgorithm_P4/src/test/java/org/mps/SwapMutationTest.java
@@ -2,6 +2,7 @@ package org.mps;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mps.mutation.SwapMutation;
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,44 +11,51 @@ public class SwapMutationTest {
 
     SwapMutation swapMutation;
 
-    @BeforeEach
-    public void setUp() {
-        swapMutation = new SwapMutation();
+    @Nested
+    class Constructor {
+        @Test
+        @DisplayName("Constructor instanciate random")
+        public void SwapMutation_CreateRandom_DoesNotThrowException() {
+
+            assertDoesNotThrow(SwapMutation::new);
+
+        }
     }
 
-    @Test
-    @DisplayName("Constructor instanciate random")
-    public void SwapMutation_CreateRandom_DoesNotThrowException() {
+    @Nested
+    class Mutate {
 
-        assertDoesNotThrow(SwapMutation::new);
+        @BeforeEach
+        public void setUp() {
+            swapMutation = new SwapMutation();
+        }
 
-    }
+        @Test
+        @DisplayName("Throws Exception if array is null")
+        public void Mutate_ArrayNull_ThrowsException() {
 
-    @Test
-    @DisplayName("Mutate array null")
-    public void Mutate_ArrayNull_ThrowsException() {
+            assertThrows(EvolutionaryAlgorithmException.class,() -> swapMutation.mutate(null));
 
-        assertThrows(EvolutionaryAlgorithmException.class,() -> swapMutation.mutate(null));
+        }
 
-    }
+        @Test
+        @DisplayName("Throws Exception if array is empty")
+        public void Mutate_EmptyArray_ThrowsException() {
+            int[] array = new int[]{};
 
-    @Test
-    @DisplayName("Mutate empty array")
-    public void Mutate_EmptyArray_ThrowsException() {
-        int[] array = new int[]{};
+            assertThrows(EvolutionaryAlgorithmException.class,() -> swapMutation.mutate(array));
 
-        assertThrows(EvolutionaryAlgorithmException.class,() -> swapMutation.mutate(array));
+        }
 
-    }
+        @Test
+        @DisplayName("Return array if input is valid")
+        public void Mutate_ValidArray_ReturnsMutatedArray() throws EvolutionaryAlgorithmException {
+            int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    @Test
-    @DisplayName("Mutate valid array")
-    public void Mutate_ValidArray_ReturnsMutatedArray() throws EvolutionaryAlgorithmException {
-        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int[] mutation = swapMutation.mutate(array);
 
-        int[] mutation = this.swapMutation.mutate(array);
+            assertEquals(array.length, mutation.length);
 
-        assertEquals(array.length, mutation.length);
-
+        }
     }
 }
