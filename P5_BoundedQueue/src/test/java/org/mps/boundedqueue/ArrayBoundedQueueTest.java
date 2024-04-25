23d818339c820f3a4e99053c7b844cef2c0f2d65
Victor Perez <vparmenta@uma.es>
2024-04-25 11:23:15 +0200

    tests constructor

--- P5_BoundedQueue/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
+++ P5_BoundedQueue/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
@@ -1,5 +1,41 @@
 package org.mps.boundedqueue;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class ArrayBoundedQueueTest {
-    
+
+    @Nested
+    class Cosntructor{
+        @Test
+        @DisplayName("Throw exception when capacity is negative.")
+        public void constructor_capacityNegative_throwException(){
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> new ArrayBoundedQueue<Integer>(-10))
+                    .withMessage("ArrayBoundedException: capacity must be positive");
+        }
+
+        @Test
+        @DisplayName("Throws exception when capacity is 0.")
+        public void constructor_capacityIs0_throwException(){
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> new ArrayBoundedQueue<Integer>(0))
+                    .withMessage("ArrayBoundedException: capacity must be positive");
+        }
+
+        @Test
+        @DisplayName("Initialize correctly the queue")
+        public void cosntructor_validCapacity_initializeCorrectlyQueue(){
+            ArrayBoundedQueue<Integer> arrayBoundedQueue = new ArrayBoundedQueue<>(4);
+
+            assertThat(arrayBoundedQueue).hasSize(0);
+            assertThat(arrayBoundedQueue.getFirst()).isEqualTo(0);
+            assertThat(arrayBoundedQueue.getLast()).isEqualTo(0);
+        }   
+    }
+
 }
