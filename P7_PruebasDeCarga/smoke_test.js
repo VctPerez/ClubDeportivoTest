fa9cd41bafaee959e014575dcd2eb1f6e80b9888
Victor Perez <vparmenta@uma.es>
2024-05-22 16:28:31 +0200

    Fix smoke_test

--- P7_PruebasDeCarga/smoke_test.js
+++ P7_PruebasDeCarga/smoke_test.js
@@ -1,19 +1,23 @@
 import http from "k6/http"
-import { sleep } from "k6"
+import { check, sleep } from "k6"
 
-export const Options = {
+export const options = {
     vus : 5,
     duration : "1m",
     thresholds: {
-    http_req_failed : [{
+        http_req_failed : [{
+        threshold: 'rate==0',
         abortOnFail: true,
-    }],
-    http_req_duration : "p(100) <= 100"
-    },
+        }],
+        http_req_duration: ['avg<100']
+    }
 }
 
 export default () =>{
     const urlRes = http.get('http://localhost:8080/medico/1');
+    
+    check(urlRes, {"El codigo de respuesta fue 200: ": () => urlRes.status == 200})
+    
     sleep(1);
 }
 
