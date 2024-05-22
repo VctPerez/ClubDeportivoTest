93a2379a1f18195b5b5851f0f87e90e9eb7c954a
José Ángel Bueno Ruiz <jabr3830@gmail.com>
2024-05-22 16:46:51 +0200

    average load & spike tests + reports

--- P7_PruebasDeCarga/average_load_test.js
+++ P7_PruebasDeCarga/average_load_test.js
@@ -1,15 +1,28 @@
 import http from "k6/http"
-import { sleep } from "k6"
+import { sleep, check } from "k6"
+
+let maxVUs = 10000
 
 export const options = {
     stages: [
-        {duration: '5m', target: 1500},
-        {duration: '20m', target: 1500},
-        {duration: '5m', target: 0}
+        {duration: '3m', target: maxVUs/2},
+        {duration: '3m', target: maxVUs/2},
+        {duration: '2m', target: 0}
     ],
+    thresholds: {
+        http_req_failed: [
+          {
+            threshold: "rate<=0.01",
+            abortOnFail: true,
+          },
+        ],
+      },
 }
 
 export default () =>{
     const urlRes = http.get('http://localhost:8080/medico/1');
+    check(urlRes, {
+        "status is 200": (r) => r.status === 200,
+    });
     sleep(1);
 }
---
+++ P7_PruebasDeCarga/spike_test.js
@@ -0,0 +1,27 @@
+import http from "k6/http"
+import { sleep, check } from "k6"
+
+let maxVUs = 10000
+
+export const options = {
+    stages: [
+        {duration: '2m', target: (0.4*maxVUs)},
+        {duration: '2m', target: 0}
+    ],
+    thresholds: {
+        http_req_failed: [
+            {
+            threshold: "rate<=0.005",
+            abortOnFail: true,
+            },
+        ],
+    },
+}
+
+export default () =>{
+    const urlRes = http.get('http://localhost:8080/medico/1');
+    check(urlRes, {
+        "status is 200": (r) => r.status === 200,
+    });
+    sleep(1);
+}
